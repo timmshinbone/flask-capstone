456e0ca43296dd770be888e4f6f3eb5354260126
Timm Schoenborn <timmschoenborn@gmail.com>
2019-12-03 22:06:35 -0600

    created routes to show all and to show individual postcards

--- resources/postcards.py
+++ resources/postcards.py
@@ -29,9 +29,27 @@ def create_postcard():
 	postcard_dict['creator'].pop('email')
 	return jsonify(data=postcard_dict, status={"code": 201, "message":"Successfully created postcard"}), 201
 
+#postcard index(show all) route
+@postcards.route('/', methods=['GET'])
+@login_required
+def get_all_postcards():
+	postcards = models.Postcard.select()
+	postcard_dicts = [model_to_dict(p) for p in postcards]
 
+	[x['creator'].pop('password') for x in postcard_dicts]
+	[x['creator'].pop('email') for x in postcard_dicts]
 
+	return jsonify(data=postcard_dicts), 200
 
+#show only one postcard route
+@postcards.route('/<id>', methods=['GET'])
+@login_required
+def get_one_postcard(id):
+	postcard = models.Postcard.get_by_id(id)
+	postcard_dict = model_to_dict(postcard)
+	postcard_dict['creator'].pop('password')
+	postcard_dict['creator'].pop('email')
+	return jsonify(data=postcard_dict, status={"code": 200, "message":"Found Postcard with id{}".format(postcard.id)}), 200
 
 
 
